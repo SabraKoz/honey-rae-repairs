6518862977c1149897cf65be0a7f5dd42885122f
Sabra Kozlina <sjkozlina@gmail.com>
2025-01-15 13:34:01 -0700

    completed part 4

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -7,18 +7,24 @@ import "./Tickets.css"
 export const TicketList = ({ currentUser }) => {
     const [allTickets, setAllTickets] = useState([])
     const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+    const [showOpenOnly, setShowOpenOnly] = useState(false)
     const [filteredTickets, setFilteredTickets] = useState([])
     const [searchTerm, setSearchTerm] = useState("")
 
     const getAndSetTickets = () => {
         getAllTickets().then((ticketsArray) => {
-            setAllTickets(ticketsArray)
+            if (currentUser.isStaff) {
+                setAllTickets(ticketsArray)
+            } else {
+                const customerTickets = ticketsArray.filter(ticket => ticket.userId === currentUser.id)
+                setAllTickets(customerTickets)
+            }
     })
 }
 
     useEffect(() => {
         getAndSetTickets()
-    }, [])
+    }, [currentUser])
 
     useEffect(() => {
         if (showEmergencyOnly) {
@@ -35,10 +41,21 @@ export const TicketList = ({ currentUser }) => {
         setFilteredTickets(foundTickets)
     }, [searchTerm, allTickets])
 
+    useEffect(() => {
+        if (showOpenOnly) {
+            const openTickets = allTickets.filter(ticket => ticket.dateCompleted === '')
+            setFilteredTickets(openTickets)
+        } else {setFilteredTickets(allTickets)}
+    }, [showOpenOnly, allTickets])
+
     return (
         <div className="tickets-container">
             <h2>Tickets</h2>
-            <TicketFilterBar setShowEmergencyOnly={setShowEmergencyOnly} setSearchTerm={setSearchTerm} />
+            <TicketFilterBar 
+                setShowEmergencyOnly={setShowEmergencyOnly} 
+                setShowOpenOnly={setShowOpenOnly}
+                setSearchTerm={setSearchTerm} 
+                currentUser={currentUser} />
             <article className="tickets">
                 {filteredTickets.map((ticketObj) => {
                     return <Ticket 
